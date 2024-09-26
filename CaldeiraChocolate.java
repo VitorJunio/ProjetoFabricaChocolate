fac8b1402ac240d37dc5b6e4174df5119afdcd1a
VitorJunio <70168288+VitorJunio@users.noreply.github.com>
2024-09-25 23:40:09 -0300

    Add files via upload

---
+++ CaldeiraChocolate.java
@@ -0,0 +1,59 @@
+
+
+public class CaldeiraChocolate {
+
+    private static CaldeiraChocolate instancia;
+
+    // Armazenar os agendamentos
+    private boolean vazia;
+    private boolean fervida;
+
+    private CaldeiraChocolate() {
+        this.vazia = true;
+        this.fervida = false;
+    }
+
+    // Obter a instância única
+    public static CaldeiraChocolate getInstancia() {
+        if (instancia == null) {
+            instancia = new CaldeiraChocolate();
+        }
+        return instancia;
+    }
+
+    // Agendar uma audiência
+    public String encher() {
+        if (this.vazia == true) {
+            this.vazia = false;
+            return "A caldeira foi preenchida com chocolate e leite.";
+        }
+        else {
+            
+            return "A caldeira está cheia.";
+        }
+    }
+    public String ferver() {
+        if (this.vazia == false) {
+            this.fervida = true;
+            return "A caldeira está fervendo.";
+        }
+        else {
+            
+            return "A caldeira está vazia.";
+        }
+    }
+    public String drenar() {
+        if (this.fervida == true) {
+            this.fervida = false;
+            this.vazia = true;
+            return "Mistura drenada.";
+        }
+        else {
+            
+            return "A caldeira não ferveu.";
+        }
+    }
+    
+
+    
+}
